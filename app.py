fd50cf76d265976951e77a22aed4e20904691dd8
maths4life <marshy1509@gmail.com>
2026-05-17 12:09:11 +0530

    Change page icon from 📈 to 💹

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from streamlit_lightweight_charts import renderLightweightCharts
 st.set_page_config(
     layout="wide",
     page_title="Quant Terminal",
-    page_icon="📈"
+    page_icon="💹"
 )
 
 # ── STYLES ────────────────────────────────────────────────────────────────────
